c197770d59dcc5cba14421665e776567aced2963
Aleksandr Kuchuk <aarexer@gmail.com>
2016-06-13 22:35:21 +0000

    Simple oop examples

---
+++ code-examples/src/main/java/core/samples/oop/IExample.java
@@ -0,0 +1,27 @@
+package core.samples.oop;
+
+public class IExample {
+    public static void main(String[] args) {
+        Person aleksandr = new Person();
+        aleksandr.setAge(10); //it's all good
+        aleksandr.setAge(-1); //we can't set it!
+    }
+}
+
+class Person {
+    private int age;
+    private String name;
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(int age) {
+        if (age < 0) throw new IllegalArgumentException("Age can't be less then zero");
+        this.age = age;
+    }
+}
