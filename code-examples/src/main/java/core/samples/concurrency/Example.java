7bd6973e3c2ae127c8ab59086f543c432d4b4867
Aleksandr Kuchuk <aarexer@gmail.com>
2016-06-21 23:04:20 +0000

    Thread interrupt examples

--- code-examples/src/main/java/core/samples/concurrency/Example.java
+++ code-examples/src/main/java/core/samples/concurrency/Example.java
@@ -1,6 +1,7 @@
 package core.samples.concurrency;
 
 import java.util.Timer;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Illustrates how we can work with threads
@@ -8,7 +9,7 @@ import java.util.Timer;
  */
 public class Example {
     public static void main(String[] args) throws InterruptedException {
-        joinExample();
+        wrongInterrupted();
     }
 
     //work with two threads
@@ -36,10 +37,12 @@ public class Example {
         te2.start();
     }
 
+    //Timer thread example
     static void startThreadByTimer() {
         new Timer().schedule(new TimerTaskExample("TimerThread", 2), 60);
     }
 
+    //Example for thread join
     static void joinExample() throws InterruptedException {
         ThreadExample te1 = new ThreadExample("My thread", 1);
         ThreadExample te2 = new ThreadExample("My thread 2", 2);
@@ -47,5 +50,44 @@ public class Example {
         te1.join();
         System.out.println("Main thread!");
         te2.start();
+    }
+
+    //Example for thread interrupt by our flag
+    static void interruptedThreadByFlag() {
+        ThreadWithFlag tFlag = new ThreadWithFlag();
+        tFlag.start();
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        tFlag.setStopFlag(true);
+    }
+
+    //Example for thread interrupt by our flag
+    static void interruptedThread() {
+        ThreadInterruptExample tFlag = new ThreadInterruptExample();
+        tFlag.start();
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        tFlag.isInterrupted();
+    }
+
+    //Example with wrong interrupt
+    //We can't stop thread without checking flag
+    static void wrongInterrupted() {
+        Thread tr = new ThreadWithoutInterruptCheck();
+        tr.start();
+        tr.interrupt();
+    }
+
+    //Example of interrupted exception while sleep
+    static void interruptWhileSleep() {
+        Thread tr = new ThreadInterruptWhileSleep();
+        tr.start();
+        tr.interrupt();
     }
 }
