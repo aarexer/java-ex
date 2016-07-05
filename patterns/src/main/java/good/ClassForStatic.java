61c84e185825296c546895032abdec63e723732e
Aleksandr Kuchuk <aarexer@gmail.com>
2016-07-05 00:48:30 +0000

    Comments

--- patterns/src/main/java/good/ClassForStatic.java
+++ patterns/src/main/java/good/ClassForStatic.java
@@ -1,5 +1,9 @@
 package good;
 
+/**
+ * Illustrate how we can create class for only static methods and fields.
+ * Good way.
+ */
 public final class ClassForStatic {
 
     public static String st_string = "Hello";
@@ -8,7 +12,18 @@ public final class ClassForStatic {
         System.out.println("Static method");
     }
 
+    /**
+     * If we create instance of class - we throw Error.
+     */
     private ClassForStatic() {
         throw new AssertionError();
+    }
+
+    /**
+     * We have error in this method! - cause we have only private constructor,
+     * and if we create instance of our class - we throw error.
+     */
+    public static ClassForStatic errorThrow() {
+        return new ClassForStatic();
     }
 }
