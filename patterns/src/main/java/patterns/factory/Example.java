8d310e8a28580e7caa033d916a109532319ecad2
Aleksandr Kuchuk <aarexer@gmail.com>
2016-07-05 00:17:18 +0000

    Factory fix

--- patterns/src/main/java/patterns/factory/Example.java
+++ patterns/src/main/java/patterns/factory/Example.java
@@ -5,6 +5,6 @@ import patterns.factory.devices.ComputerType;
 public class Example {
     public static void main(String[] args) {
         ComputerFactory factory = new ComputerFactory();
-        System.out.println(factory.getComputer(ComputerType.DESKTOP));
+        System.out.println(factory.getComputer(ComputerType.DESKTOP).getManufacturer());
     }
 }
