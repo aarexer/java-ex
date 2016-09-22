37f3a56bc4ab60c579cd1ce7b24e3c9de3f77989
AKuchuk <akuchuk@at-consulting.ru>
2016-09-22 19:21:19 +0300

    Own parser and tests

--- code-examples/src/main/java/compare/parsers/csv/Examples.java
+++ code-examples/src/main/java/compare/parsers/csv/Examples.java
@@ -6,29 +6,47 @@ import utils.generators.CSVFileGenerator;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+import static java.lang.Double.NaN;
 
 public class Examples {
     private static Logger LOGGER = LogManager.getLogger();
+    private static String name = "TEST.csv";
+    private static int SIZE_OF_BUFFER = 31457280;
 
-    public static void main(String[] args) throws IOException {
-        String name = "TEST.csv";
-        int numberOfLines = 100_000;
-        CSVFileGenerator csvFileGenerator = new CSVFileGenerator(',', 100);
-        csvFileGenerator.generateNewFile(name, numberOfLines);
+    public static void main(String[] args) throws Exception {
+        openCSVTestTimeLineByLineParsing(10, 1000);
+    }
 
-        LOGGER.info("Generate {} file with {} columns and it size: {} mb.", name, numberOfLines, new File(name).length() / 1024);
+    private static void createFile(int columns, int rows) {
+        CSVFileGenerator csvFileGenerator = new CSVFileGenerator(',', columns);
+        csvFileGenerator.generateNewFile(name, rows);
 
-        System.out.println("---------------------------");
-        new OpenCSVParser(name).parseByLineWithoutBuffer();
-        System.out.println("---------------------------");
-        new OpenCSVParser(name).parseByLineWithBuffer();
-        System.out.println("---------------------------");
-        new OpenCSVParser(name).parseFileAsList();
+        double sizeInBytes = new File(name).length();
 
-        System.out.println("---------------------------");
-        System.out.println("-----------Apache----------");
-        new ApacheCommonsCSVParser(name).parseFileWithBufferAndRecords();
-        System.out.println("---------------------------");
-        new ApacheCommonsCSVParser(name).parseFileWithIterator();
+        LOGGER.info("Generate file with {} columns, {} rows.", columns, rows);
+
+        if ((int) sizeInBytes / (1024 * 1024) > 0) {
+            LOGGER.info("File size is {} mb", sizeInBytes / (1024 * 1024));
+        } else if ((int) sizeInBytes / 1024 > 0) {
+            LOGGER.info("File size is {} kb", sizeInBytes / 1024);
+        } else {
+            LOGGER.info("File size is {} bytes", sizeInBytes);
+        }
+    }
+
+    private static void openCSVTestTimeLineByLineParsing(int columns, int rows) throws IOException {
+        createFile(columns, rows);
+        List<Long> times = new LinkedList<>();
+
+        long attempt = 0;
+        while (attempt <= 200_000) {
+            times.add(new OpenCSVParser(name).parseLineByLine());
+            attempt++;
+        }
+
+        LOGGER.info("Median: {} mills, average: {} mills", times.get(times.size() / 2), times.stream().mapToLong(Long::longValue).average().orElse(NaN));
     }
 }
--- code-examples/src/main/java/compare/parsers/csv/SuperCSVParser.java
+++ code-examples/src/main/java/compare/parsers/csv/SuperCSVParser.java
@@ -1,4 +1,62 @@
 package compare.parsers.csv;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.supercsv.io.CsvListReader;
+import org.supercsv.io.ICsvListReader;
+import org.supercsv.prefs.CsvPreference;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+import static java.lang.Double.NaN;
+
 public class SuperCSVParser {
+    private static Logger LOGGER = LogManager.getLogger();
+    private FileReader fileReader;
+    private static int SIZE_OF_BUFFER = 31457280;
+
+    public SuperCSVParser(String filename) {
+        try {
+            fileReader = new FileReader(filename);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void readWithCsvListReaderWithBuffer() throws Exception {
+        LOGGER.info("Creating parser with buffer....");
+        ICsvListReader listReader = new CsvListReader(new BufferedReader(fileReader, SIZE_OF_BUFFER), CsvPreference.STANDARD_PREFERENCE);
+        parseFileWithCsvList(listReader);
+
+        listReader.close();
+    }
+
+    public void readWithCsvListReaderWithoutBuffer() throws Exception {
+        LOGGER.info("Creating parser without buffer....");
+        ICsvListReader listReader = new CsvListReader(fileReader, CsvPreference.STANDARD_PREFERENCE);
+        parseFileWithCsvList(listReader);
+
+        listReader.close();
+    }
+
+
+    private void parseFileWithCsvList(ICsvListReader listReader) throws IOException {
+        List<String> customerList;
+        List<Long> times = new LinkedList<>();
+        long start = System.currentTimeMillis();
+        long startTmp = start;
+
+        while ((customerList = listReader.read()) != null) {
+            times.add(System.currentTimeMillis() - startTmp);
+            startTmp = System.currentTimeMillis();
+        }
+
+        LOGGER.info("File parsed by: {}", System.currentTimeMillis() - start);
+        LOGGER.info("Average time of parsing one line: {}", times.stream().mapToLong(Long::longValue).average().orElse(NaN));
+    }
 }
