7cc8f3c892ffdb1bafe0a5e584dc4e50a3c22aec
aarexer <aarexer@gmail.com>
2017-01-05 16:34:49 +0300

    Simple examples for read and write to csv file

---
+++ code-snippets/src/main/java/parsers/csv/model/Student.java
@@ -0,0 +1,31 @@
+package parsers.csv.model;
+
+public class Student {
+    private long id;
+    private String name;
+    private String surname;
+    private int age;
+
+    public Student(long id, String name, String surname, int age) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
