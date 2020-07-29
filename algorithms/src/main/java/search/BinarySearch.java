c62b5c486f7118a6ee0332ce2ce48784f9765868
aarexer <aarexer@gmail.com>
2020-07-30 00:00:31 +0300

    - Fix

--- algorithms/src/main/java/search/BinarySearch.java
+++ algorithms/src/main/java/search/BinarySearch.java
@@ -1,26 +1,50 @@
 package search;
 
 public class BinarySearch {
-    public static boolean search(int[] arr, int element) {
+    public static int search(final int[] arr, final int element) {
         if (arr.length == 0) {
-            return false;
+            return -1;
         }
+
         return search(arr, element, 0, arr.length - 1);
     }
 
-    private static boolean search(int[] arr, int element, int left, int right) {
+    private static int search(final int[] arr, final int element, final int left, final int right) {
         if (left > right) {
-            return false;
+            return -1;
         }
 
         final int mid = (left + right) / 2;
 
         if (arr[mid] == element) {
-            return true;
+            return mid;
         } else if (arr[mid] > element) {
             return search(arr, element, left, mid - 1);
         } else {
             return search(arr, element, mid + 1, right);
         }
+    }
+
+    public static int searchIter(final int[] arr, final int key) {
+        if (arr.length == 0) {
+            return -1;
+        }
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (arr[mid] == key) {
+                return mid;
+            } else if (arr[mid] < key) {
+                left = mid + 1;
+            } else if (arr[mid] > key) {
+                right = mid - 1;
+            }
+        }
+
+        return -1;
     }
 }
--- algorithms/src/test/java/search/TestBinarySearch.java
+++ algorithms/src/test/java/search/TestBinarySearch.java
@@ -5,44 +5,44 @@ import org.junit.Test;
 
 public class TestBinarySearch {
     @Test
-    public void shouldReturnTrueOnArrayWithOneElement() {
+    public void shouldReturnZeroIndexOnArrayWithOneElement() {
         final int[] arr = {1};
-        Assert.assertEquals(BinarySearch.search(arr, 1), true);
+        Assert.assertEquals(BinarySearch.search(arr, 1), 0);
     }
 
     @Test
-    public void shouldReturnTrueOnArrayWithTwoElements() {
+    public void shouldReturnZeroIndexOnArrayWithTwoElementsWhenSoughtIsFirst() {
         final int[] arr = {1, 2};
-        Assert.assertEquals(BinarySearch.search(arr, 1), true);
+        Assert.assertEquals(BinarySearch.search(arr, 1), 0);
     }
 
     @Test
-    public void shouldReturnFalseOnEmptyArray() {
+    public void shouldReturnNegativeOnEmptyArray() {
         final int[] arr = {};
-        Assert.assertEquals(BinarySearch.search(arr, 1), false);
+        Assert.assertEquals(BinarySearch.search(arr, 1), -1);
     }
 
     @Test
-    public void shouldReturnFalseOnArrayWhichDoesntContainElementInRightSide() {
+    public void shouldReturnNegativeOnArrayWhichDoesntContainElementInRightSide() {
         final int[] arr = {1, 2, 3, 4, 5};
-        Assert.assertEquals(BinarySearch.search(arr, 7), false);
+        Assert.assertEquals(BinarySearch.search(arr, 7), -1);
     }
 
     @Test
-    public void shouldReturnFalseOnArrayWhichDoesntContainElementInLeftSide() {
+    public void shouldReturnNegativeOnArrayWhichDoesntContainElementInLeftSide() {
         final int[] arr = {1, 2, 3, 4, 5};
-        Assert.assertEquals(BinarySearch.search(arr, 0), false);
+        Assert.assertEquals(BinarySearch.search(arr, 0), -1);
     }
 
     @Test
-    public void shouldReturnTrueOnArrayWhichContainsElementInRightSide() {
+    public void shouldReturnSixOnArrayWithSoughtElementInLastPosition() {
         final int[] arr = {1, 2, 3, 4, 5, 6, 7};
-        Assert.assertEquals(BinarySearch.search(arr, 7), true);
+        Assert.assertEquals(BinarySearch.search(arr, 7), 6);
     }
 
     @Test
-    public void shouldReturnTrueOnArrayWhichContainsElementInLeftSide() {
+    public void shouldReturnOneOnArrayWhichSoughtElementInSecondPosition() {
         final int[] arr = {1, 2, 3, 4, 5, 6, 7};
-        Assert.assertEquals(BinarySearch.search(arr, 2), true);
+        Assert.assertEquals(BinarySearch.search(arr, 2), 1);
     }
 }
